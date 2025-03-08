55a70885fc39733ee54e5ed6c81b8d08d5adaad8
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2025-03-08 12:38:12 -0500

    beyond1lte: remove unsupported official flag

--- infinity_beyond1lte.mk
+++ infinity_beyond1lte.mk
@@ -43,7 +43,6 @@ TARGET_BUILD_VIMUSIC := true
 TARGET_SUPPORTS_QUICK_TAP := true
 WITH_GAPPS := true
 TARGET_HAS_UDFPS := true
-TARGET_BUILD_GOOGLE_TELEPHONY := true
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := infinity_beyond1lte
